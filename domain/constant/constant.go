8076630d0da314a73762750c38913397a8e8722b
powenyu <powenyu0706@gmail.com>
2023-02-26 04:40:03 +0800

    add chat gpt

---
+++ domain/constant/constant.go
@@ -0,0 +1,14 @@
+package constant
+
+type ChannelList string
+
+const (
+	ChatChannel ChannelList = "1079128259230453761"
+)
+
+type UserList string
+
+const (
+	Melody UserList = "833949640705572874"
+	Powen  UserList = "428929512193916928"
+)
